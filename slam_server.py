a6e85aea11220dfecfbc56dbdb05374ec686a28b
AshwinSakhare <asakhare@zdatainc.com>
2024-08-09 13:55:02 -0400

    update server

--- slam_server.py
+++ slam_server.py
@@ -1,18 +1,33 @@
 import base64
 import pickle
-import threading
 from flask import Flask, request, Response, jsonify
-
-from visual_slam import start_slam, get_frames
+from visual_slam import VisualSLAMSystem
+from dataset import VideoDataset, DatasetType
+from camera import PinholeCamera
 
 app = Flask(__name__)
+slam_system = VisualSLAMSystem()
 
 
 @app.route('/slam/start', methods=['POST'])
-def start():
-    # Start SLAM in a new thread
-    thread = threading.Thread(target=start_slam)
-    thread.start()
+def start_slam():
+    data = request.json
+    cam_intrinsics = data['cam_intrinsics']
+    video_filepath = data['video_filepath']
+
+    cam = PinholeCamera(cam_intrinsics['Camera.width'],
+                        cam_intrinsics['Camera.height'],
+                        cam_intrinsics['Camera.fx'],
+                        cam_intrinsics['Camera.fy'],
+                        cam_intrinsics['Camera.cx'],
+                        cam_intrinsics['Camera.cy'],
+                        cam_intrinsics['DistCoef'],
+                        cam_intrinsics['Camera.fps']
+                        )
+
+    dataset = VideoDataset(path=video_filepath, name='video', type=DatasetType.VIDEO)
+
+    slam_system.start(cam=cam, dataset=dataset)
 
     return Response(status=204)
 
@@ -20,13 +35,12 @@ def start():
 def serialize_frames(frames):
     serialized_pred = pickle.dumps(frames)
     base64_pred = base64.b64encode(serialized_pred).decode('utf-8')
-
     return jsonify(serialized_data=base64_pred)
 
 
 @app.route('/slam/get_last_frame', methods=['POST'])
 def get_last_frame():
-    frames = get_frames(idx=-1)
+    frames = slam_system.get_frames(idx=-1)
 
     return serialize_frames(frames)
 
@@ -35,14 +49,14 @@ def get_last_frame():
 def get_frame_by_idx():
     data = request.json
     idx = data['idx']
-    frames = get_frames(idx=idx)
+    frames = slam_system.get_frames(idx=idx)
 
     return serialize_frames(frames)
 
 
 @app.route('/slam/get_all_frames', methods=['POST'])
 def get_all_frames():
-    frames = get_frames()
+    frames = slam_system.get_frames()
 
     return serialize_frames(frames)
 
--- visual_slam.py
+++ visual_slam.py
@@ -1,109 +1,109 @@
-#!/usr/bin/env -S python3 -O
-"""
-* This file is part of PYSLAM 
-*
-* Copyright (C) 2016-present Luigi Freda <luigi dot freda at gmail dot com> 
-*
-* PYSLAM is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* PYSLAM is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with PYSLAM. If not, see <http://www.gnu.org/licenses/>.
-"""
-
 import time
-
 from camera import PinholeCamera
-from config import Config
-from dataset import dataset_factory
+from dataset import VideoDataset, Dataset
 from feature_tracker import feature_tracker_factory, FeatureTrackerTypes
 from feature_tracker_configs import FeatureTrackerConfigs
 from slam import Slam
-
-slam = None
-
-def start_slam():
-    config = Config()
-
-    dataset = dataset_factory(config.dataset_settings)
-
-    cam = PinholeCamera(config.cam_settings['Camera.width'], config.cam_settings['Camera.height'],
-                        config.cam_settings['Camera.fx'], config.cam_settings['Camera.fy'],
-                        config.cam_settings['Camera.cx'], config.cam_settings['Camera.cy'],
-                        config.DistCoef, config.cam_settings['Camera.fps'])
-
-    num_features = 2000
-
-    tracker_type = FeatureTrackerTypes.DES_BF
-    tracker_config = FeatureTrackerConfigs.TEST
-    tracker_config['num_features'] = num_features
-    tracker_config['tracker_type'] = tracker_type
-    feature_tracker = feature_tracker_factory(**tracker_config)
-
-    # create SLAM object
-    slam = Slam(cam, feature_tracker, None)
-
-    # create Viewer object
-    # viewer3D = Viewer3D()
-
-    img_id = 0  #180, 340, 400   # you can start from a desired frame id if needed
-    while dataset.isOk():
-        print('..................................')
-        print('image: ', img_id)
-
-        img = dataset.getImageColor(img_id)
-
-        timestamp = dataset.getTimestamp()          # get current timestamp
-        next_timestamp = dataset.getNextTimestamp() # get next timestamp
-        frame_duration = next_timestamp-timestamp
-
-        time_start = time.time()
-        slam.track(img, img_id, timestamp)  # main SLAM function
-        duration = time.time()-time_start
-
-        # img_draw = slam.map.draw_feature_trails(img)
-        # cv2.imshow('Camera', img_draw)
-        # viewer3D.draw_map(slam)
-
-        if(frame_duration > duration):
-            print('sleeping for frame')
-            time.sleep(frame_duration-duration)
-
-        img_id += 1
-
-    slam.quit()
+import threading
 
 
-def get_frames(idx: int = None):
-    frames = []
+class VisualSLAMSystem:
+    def __init__(self, num_features=2000, tracker_type=FeatureTrackerTypes.DES_BF):
+        tracker_config = FeatureTrackerConfigs.TEST
+        tracker_config['num_features'] = num_features
+        tracker_config['tracker_type'] = tracker_type
+        self.feature_tracker = feature_tracker_factory(**tracker_config)
+        self.slam = None
+        self.dataset = None
+        self.cam = None
 
-    if idx is not None:
-        # Handle single frame retrieval
+    def start(self, cam: PinholeCamera, dataset: Dataset):
+        self.cam = cam
+        self.dataset = dataset
+
+        self.slam = Slam(self.cam, self.feature_tracker, None)
+
+        thread = threading.Thread(target=self._run_slam)
+        thread.start()
+
+    def _run_slam(self):
+        img_id = 0
+        while self.dataset.isOk():
+            print(f'Processing image: {img_id}')
+
+            img = self.dataset.getImageColor(img_id)
+            timestamp = self.dataset.getTimestamp()
+            next_timestamp = self.dataset.getNextTimestamp()
+            frame_duration = next_timestamp - timestamp
+
+            time_start = time.time()
+            self.slam.track(img, img_id, timestamp)
+            duration = time.time() - time_start
+
+            if frame_duration > duration:
+                time.sleep(frame_duration - duration)
+
+            img_id += 1
+
+        self.stop()
+
+    def stop(self):
+        if self.slam:
+            self.slam.quit()
+
+    def get_frames(self, idx=None):
+        frames = []
+        if idx is not None:
+            frame = self._extract_frame_data(idx)
+            return [frame]
+        else:
+            for idx in range(len(self.slam.map.frames)):
+                frame = self._extract_frame_data(idx)
+                frames.append(frame)
+
+        return frames
+
+    def _extract_frame_data(self, idx):
         frame = {}
-        frame_data = slam.map.frames[idx]
+        frame_data = self.slam.map.frames[idx]
         good_indexes = [i for i, p in enumerate(frame_data.points) if p is not None and not p.is_bad]
 
         frame['points'] = [frame_data.points[i].pt for i in good_indexes]
         frame['kps'] = [frame_data.kps[i] for i in good_indexes]
         frame['pose'] = frame_data.Twc
+        frame['id'] = frame_data.id
 
-        return [frame]
-
-    else:
-        # Handle retrieval of all frames
-        for idx in range(len(slam.map.frames)):
-            frame = get_frames(idx=idx)[0]  # Get the single frame from the list
-            frames.append(frame)
-
-    return frames
+        return frame
 
 
 if __name__ == "__main__":
-    start_slam()
+    slam_system = VisualSLAMSystem()
+
+    cam_intrinsics = {
+        'width': 3840,
+        'height': 2160,
+        'fx': 2376.5625,
+        'fy': 2376.5625,
+        'cx': 1920,
+        'cy': 1080,
+        'DistCoef': [0.13, -0.24, 0.0, 0.0, 0.0],
+        'fps': 59.96
+    }
+
+    from config import Config
+    from dataset import dataset_factory
+
+    config = Config()
+    dataset = dataset_factory(config.dataset_settings)
+
+    cam = PinholeCamera(config.cam_settings['Camera.width'],
+                        config.cam_settings['Camera.height'],
+                        config.cam_settings['Camera.fx'],
+                        config.cam_settings['Camera.fy'],
+                        config.cam_settings['Camera.cx'],
+                        config.cam_settings['Camera.cy'],
+                        config.DistCoef,
+                        config.cam_settings['Camera.fps']
+                        )
+
+    slam_system.start(cam=cam, dataset=dataset)
